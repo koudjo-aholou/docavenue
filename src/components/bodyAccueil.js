d6790ff7354b63b1b365ce7d40b79deb6826cc59
Koudjo <ah.koudjo@gmail.com>
2019-08-04 21:27:59 +0200

    Optimisation css BodyAccueil + convert class vers functionnal component
    optimisation css + ajout font roboto

--- src/components/bodyAccueil.js
+++ src/components/bodyAccueil.js
@@ -4,51 +4,96 @@ import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from 'react-nati
 const { width, height } = Dimensions.get('window');
 import LottieView from 'lottie-react-native';
 
-export default class BodyAccueil extends React.Component {
+export default function BodyAccueil(props) {
+  const { 
+    container, 
+    width85per, 
+    elements, 
+    contLottie, 
+    marginBotPercentage, 
+    lottieView, 
+    contBouton, 
+    bouton, 
+    textBouton 
+  } = styles;
+  //console.warn("width of the device : ",width, "height of the device :", height);
 
-  render() {
-    const {container, lottie, bouton, textBouton} = styles;
-    console.warn("width of the device : ",width, "height of the device :", height);
-
-    return (
-        <View style={container}>
-          <View style={lottie}>
-            <LottieView source={require('../assets/lottie-accueil-news.json')} autoPlay loop /> 
+  return (
+    <View style={container}>
+      <View style={width85per}>
+        <View style={elements}>
+          <View style={contLottie}>
+            <View style={marginBotPercentage} />
+            <LottieView 
+              source={require('../assets/lottie-accueil-news.json')} 
+              style={lottieView} 
+              autoPlay loop
+              resizeMode="cover" 
+            />
           </View>
-            <View>
-              <TouchableOpacity
-                  style={bouton}
-                  underlayColor='#fff'
-                  onPress={() => this.props.navigation.navigate('Posts') }
-                >
-                <Text style={textBouton}>Découvrir les témoignages</Text>
-              </TouchableOpacity>  
-            </View>
+          <View style={contBouton}>
+             <TouchableOpacity
+                style={bouton}
+                underlayColor='#fff'
+                onPress={() => props.navigation.navigate('Posts') }
+              >
+              <Text style={textBouton}>Découvrir les posts</Text>
+            </TouchableOpacity> 
+          </View>
         </View>
-    );
-  }
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container:{
     flex:3, 
-    backgroundColor : "#44a7b7", 
-    justifyContent:"flex-end"
+    backgroundColor : "#44a7b7",
+    alignItems:"center",
+    justifyContent:"center"
   },
-  lottie:{
-    flex:2
+  width85per:{
+    width:"85%",
+    flex:2,
+    //permet espace en haut et espace en bas
+    justifyContent:"space-around",
+    //backgroundColor:"purple"
+  },
+  elements:{
+    //backgroundColor:'green', 
+    width:'100%', 
+    height:"90%", 
+    alignItems:'center', 
+    justifyContent:'space-between'
+  },
+  conLottie:{
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  marginBotPercentage:{
+    // Remplacement de MarginBottom : height * xxx de conLottie
+    backgroundColor:'transparent', 
+    height:'42%'
+  },
+  lottieView:{
+    width:160, 
+    height:150,
+    //backgroundColor:'brown'
+  },
+  contBouton:{
+    height:'10%', 
+    width:"80%",
+    //backgroundColor:'pink',
+    justifyContent:'center',
   },
   bouton:{
-   //marginLeft & marginRight de 40 
-    marginRight:width * 0.1111,
-    marginLeft:width * 0.1111,
-    marginTop:20,
-    marginBottom:20,
-    paddingTop:10,
-    paddingBottom:10,
+    //paddingTop paddingBottom 10
+    paddingTop:height * 0.0156 ,
+    paddingBottom:height * 0.0156,
     borderRadius:30,
+    width:"100%",
     borderWidth: 1,
-    width:"75%",
     borderColor: '#B79944',
     backgroundColor:'#B79944'
   },
@@ -57,7 +102,8 @@ const styles = StyleSheet.create({
     //paddingLeft & paddingRight de 10
     paddingLeft :width * 0.0277,
     paddingRight :width * 0.0277,
-    color:'#fff'
-  }
-    
+    color:'#fff',
+    fontSize:15,
+    fontFamily: 'Roboto-Regular'
+  } 
 });
